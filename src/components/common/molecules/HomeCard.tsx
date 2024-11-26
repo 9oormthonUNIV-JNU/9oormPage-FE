d3ed95ee5014864551947c0288489daa79b758f1
kyungmin <02dlrudals@gmail.com>
2024-11-26 17:09:32 +0900

    fix: HomeCard 수정

--- src/components/common/molecules/HomeCard.tsx
+++ src/components/common/molecules/HomeCard.tsx
@@ -1,8 +1,18 @@
-import icon_pencil from "../../../../public/icons/icon_pencil.svg";
-import img_flower from "../../../../public/images/img_flower.svg";
 import CustomCard from "../atoms/CustomCard";
 
-const HomeCard = () => {
+type HomeCardProps = {
+  icon: string;
+  title: string;
+  discription: string;
+  img: string;
+};
+
+const HomeCard: React.FC<HomeCardProps> = ({
+  icon,
+  title,
+  discription,
+  img,
+}) => {
   return (
     <CustomCard
       className="lg:w-96 md:w-72 h-[440px] flex flex-col overflow-hidden transition-all duration-300 ease-out hover:scale-[101%] hover:shadow-2xl"
@@ -17,18 +27,14 @@ const HomeCard = () => {
     >
       <div className="px-5 pt-5 flex flex-col flex-grow">
         <div className="flex gap-3">
-          <img src={icon_pencil} className="w-8" />
-          <span className="text-h3 truncate">네트워킹 Networking</span>
+          <img src={icon} className="w-8" />
+          <span className="text-h3 truncate">{title}</span>
         </div>
-        <div className="text-b3 mt-3 h-44">
-          9UAP은 ‘9oormthonUNIV After Party’의 약자로, 해커톤 대상 및 최우수
-          수상자들 그리고 현직 전문가들의 세미나와 마무리 회고를 진행하는
-          행사입니다.
-        </div>
+        <div className="text-b3 mt-3 h-44">{discription}</div>
       </div>
       {/* 이미지 아래로 밀어내기 */}
-      <div className="bg-black w-full h-[190px]">
-        <img className="w-full h-full object-cover" src={img_flower} />
+      <div className="w-full h-[190px]">
+        <img className="w-full h-full object-cover" src={img} />
       </div>
     </CustomCard>
   );

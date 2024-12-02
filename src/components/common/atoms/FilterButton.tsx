ccd16145f8e6dc62a0d86cf192994032dfcd16cc
kyungmin <02dlrudals@gmail.com>
2024-12-02 15:01:01 +0900

    fix

--- src/components/common/atoms/FilterButton.tsx
+++ src/components/common/atoms/FilterButton.tsx
@@ -27,12 +27,11 @@ const FilterButton: React.FC<FilterButtonProps> = ({
     onClick(option);
   };
 
-  // 버튼의 너비를 계산하여 상태로 저장
   useEffect(() => {
     if (buttonRef.current) {
       setButtonWidth(buttonRef.current.offsetWidth);
     }
-  }, [selectedOption]); // selectedOption이 변경될 때마다 버튼의 너비를 업데이트
+  }, [selectedOption]);
 
   return (
     <div className="relative">
@@ -40,16 +39,15 @@ const FilterButton: React.FC<FilterButtonProps> = ({
         ref={buttonRef}
         onClick={toggleDropdown}
         className="flex items-center justify-center gap-2 px-5 py-3 h-[55px] rounded-full bg-[#F7F7F7] border border-[#E5E5E5] cursor-pointer hover:border-[#e5e5e5] hover:bg-[#e5e5e5] focus:bg-transparent focus:outline-none"
-        style={{ minWidth: buttonWidth ? `${buttonWidth}px` : "auto" }} // 버튼의 너비가 자동으로 결정되도록 min-width 사용
+        style={{ minWidth: buttonWidth ? `${buttonWidth}px` : "auto" }}
       >
         <span className="text-b3 text-center">{selectedOption}</span>
-        {/* 아이콘을 오른쪽 끝에 고정 */}
         <img src={icon_down} alt="down arrow" className="ml-auto" />
       </button>
       {isOpen && (
         <div
           className="absolute bg-[#F7F7F7] rounded-[10px] z-10 p-3"
-          style={{ minWidth: buttonWidth ? `${buttonWidth}px` : "auto" }} // 드롭다운 메뉴도 버튼과 동일한 너비로 유지
+          style={{ minWidth: buttonWidth ? `${buttonWidth}px` : "auto" }}
         >
           {options.map((option) => (
             <div
